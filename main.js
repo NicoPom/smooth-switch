3d9bc82c468aa89e1fabb479f20bc82780271383
Nico Pom <niconp7@yahoo.fr>
2023-02-17 23:45:40 +0100

    did a lot

--- main.js
+++ main.js
@@ -4,25 +4,61 @@ import "./style.css";
 
 gsap.registerPlugin(Flip);
 
-const container = document.querySelector(".container");
+const mainContainer = document.querySelector(".main-container");
+const switchContainer = document.querySelector(".switch-container");
 const highlight = document.querySelector(".highlight");
+const switches = switchContainer.querySelectorAll(".switch");
+const firstSwitch = switches[0];
 
-// Using event delegation to listen for mouseover events on the container
-container.addEventListener("mouseover", (event) => {
-  // Find the switch that was hovered
+//
+gsap.set(switchContainer, {
+  width: firstSwitch.offsetWidth + "px",
+  overflow: "hidden",
+  translate: 0,
+});
+
+mainContainer.addEventListener("mouseenter", () => {
+  gsap.to(mainContainer, { maxWidth: "100%", duration: 0.5 });
+  gsap.to(switchContainer, {
+    width: "100%",
+    overflow: "hidden",
+    translate: 0,
+    duration: 0.5,
+  });
+});
+
+mainContainer.addEventListener("mouseleave", () => {
+  const selectedSwitch = switchContainer.querySelector(".selected");
+  const targetSwitch = selectedSwitch || firstSwitch;
+  gsap.to(switchContainer, {
+    width: targetSwitch.offsetWidth + "px",
+    duration: 0.3,
+  });
+});
+
+switchContainer.addEventListener("mouseover", (event) => {
   const switchEl = event.target.closest(".switch");
   if (!switchEl) return;
 
-  // Move the highlight between the switches using Flip
   const highlightState = Flip.getState(highlight);
   switchEl.appendChild(highlight);
-  Flip.from(highlightState, {
+  Flip.from(highlightState, { duration: 0.3 });
+
+  switches.forEach((el) => el.classList.remove("highlighted"));
+  switchEl.classList.add("highlighted");
+});
+
+switchContainer.addEventListener("click", (event) => {
+  const switchEl = event.target.closest(".switch");
+  if (!switchEl) return;
+
+  switches.forEach((el) => el.classList.remove("selected"));
+  switchEl.classList.add("selected");
+
+  gsap.to(switchContainer, {
+    overflow: "visible",
+    translate: -switchEl.offsetLeft + "px",
+    width: switchEl.offsetWidth + "px",
     duration: 0.3,
   });
-
-  // Change the text opacity of the hovered switch
-  container.querySelectorAll(".text").forEach((el) => {
-    el.classList.remove("highlighted");
-  });
-  switchEl.querySelector(".text").classList.add("highlighted");
 });
