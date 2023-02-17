1bff8c91ccd1a96058f5e0e414b1dbe1b6a4fecc
Nico Pom <niconp7@yahoo.fr>
2023-02-17 16:57:32 +0100

    highlight done

--- main.js
+++ main.js
@@ -6,14 +6,27 @@ gsap.registerPlugin(Flip);
 
 const switches = document.querySelectorAll(".switch");
 const highlight = document.querySelector(".highlight");
+const texts = document.querySelectorAll(".text");
 
 switches.forEach((switchEl) => {
   switchEl.addEventListener("mouseover", () => {
     // Move the higlight between the switches
-    const state = Flip.getState(highlight);
+    const hihlightState = Flip.getState(highlight);
     switchEl.appendChild(highlight);
-    Flip.from(state, {
-      duration: 1,
+    Flip.from(hihlightState, {
+      duration: 0.3,
     });
+
+    // // change the text opacity
+    // gsap.to(texts, {
+    //   opacity: 0.5,
+    //   duration: 0.3,
+    // });
+
+    // gsap.to(switchEl.querySelector(".text"), {
+    //   opacity: 1,
+
+    //   delay: 0.1,
+    // });
   });
 });
